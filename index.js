78efc90ac6620b24026a11c60b59d7d7c10f0ec7
mtalalned <muhammadtalalned@gmail.com>
2025-01-14 19:11:53 +0500

    back end completed

--- index.js
+++ index.js
@@ -10,22 +10,22 @@ dotenv.config()
 
 app.use (express.json())
 
-const allowedOrigins = ['https://shopping-cart-with-custom-back-end.vercel.app'];  // Add frontend URLs here
+// const allowedOrigins = ['https://shopping-cart-with-custom-back-end.vercel.app'];  // Add frontend URLs here
 
-app.use(cors({
-    origin: (origin, callback) => {
-        if (allowedOrigins.includes(origin) || !origin) {
-            callback(null, true);  // Allow the request
-        } else {
-            callback(new Error('Not allowed by CORS'));  // Reject the request
-        }
-    },
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'], // Allow Authorization header
-    credentials: true, // Allow sending cookies
-}));
+// app.use(cors({
+//     origin: (origin, callback) => {
+//         if (allowedOrigins.includes(origin) || !origin) {
+//             callback(null, true);  // Allow the request
+//         } else {
+//             callback(new Error('Not allowed by CORS'));  // Reject the request
+//         }
+//     },
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type', 'Authorization'], // Allow Authorization header
+//     credentials: true, // Allow sending cookies
+// }));
 
-// app.use(cors())
+app.use(cors())
 app.use(cookieParser())
 app.use ('/api/v1' , UserRouter)
 
